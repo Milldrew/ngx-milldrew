1b1602208b0e05460dfeb94a4a926a9c92ae48ea
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-03 20:30:49 -0600

    📅  | Tue May  3 20:30:48 MDT 2022

---
+++ projects/milldrews-lib/milldrews-schematics/ng-add/index.ts
@@ -0,0 +1,12 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function ngAdd(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    _context.logger.info('Installing dependencies...');
+    _context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
+}
